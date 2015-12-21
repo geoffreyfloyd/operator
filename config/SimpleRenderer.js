80d24e930c71553df18fc75b7a741706e6c8f706
Geoff Manning <geoff.manning.sd@gmail.com>
2015-12-20 17:46:19 -0700

    extend to doozy api

--- config/SimpleRenderer.js
+++ config/SimpleRenderer.js
@@ -6,8 +6,17 @@ function SimpleRenderer(options) {
     this.html = html.replace('SCRIPT_URL', options.scriptUrl);
 }
 
-SimpleRenderer.prototype.render = function(_path, _readItems, callback) {
+SimpleRenderer.prototype.setScriptUrl = function (scriptUrl) {
+    this.html = html.replace('SCRIPT_URL', scriptUrl);
+};
+
+SimpleRenderer.prototype.render = function (_path, _readItems, callback) {
     callback(null, this.html);
+};
+
+SimpleRenderer.prototype.renderWithScript = function (scriptUrl, _path, _readItems, callback) {
+    var otfHtml = html.replace('SCRIPT_URL', scriptUrl);
+    callback(null, otfHtml);
 };
 
 module.exports = SimpleRenderer;
