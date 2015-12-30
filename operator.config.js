84c3ee90829941aaa28c73a226a549c42dc876f4
geoffreyfloyd <geoffrey.floyd@hoomanlogic.com>
2015-12-29 23:38:31 -0700

    operator refactor

---
+++ operator.config.js
@@ -0,0 +1,8 @@
+module.exports = exports = {
+    name: 'Geoffrey Floyd',
+    email: 'geoff.manning.sd@gmail.com',
+    repoPath: '../../tdb',
+    remoteUrl: 'https://github.com/geoffreyfloyd/tdb.git',
+    //plugins: ['../api/cmd-api', '../api/gnodes-api', '../api/research-api', '../api/utils-api']
+    plugins: ['doozy/src/Client/plugins/operator']
+};
